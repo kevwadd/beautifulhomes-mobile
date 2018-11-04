6b92c08aeebc49471e649b88aee2b041a258d502
Kevin Waddell <kevwaddell@mac.com>
2018-11-04 18:09:33 +0000

    Add slide in button to footer links

--- parts/global/footer-nav.php
+++ parts/global/footer-nav.php
@@ -5,4 +5,5 @@ $work_with_us_pg = get_page_by_path('work-with-us');
 <a href="<?php echo get_permalink($rate_my_rooms_pg->ID); ?>" class="flex-fill"><i class="fa fa-star fa-lg"></i><span><?php echo get_the_title($rate_my_rooms_pg->ID); ?></span></a>
 <a href="<<?php echo get_permalink($work_with_us_pg->ID); ?>" class="flex-fill"><i class="fa fa-handshake-o fa-lg"></i><span>Work<br>with us</span></a>
 <a href="mailto:<?php echo $main_email; ?>" class="flex-fill"><i class="fa fa-envelope fa-lg"></i><span>Get in<br>touch</span></a>
+<a href="#" id="subscribe-btn" class="flex-fill"><i class="fa fa-newspaper-o fa-lg"></i><span>Subscribe<br>to blog</span></a>
 <a href="#" data-toggle="modal" id="search-btn" data-target="#search-modal" target="_blank" class="flex-fill"><i class="fa fa-search fa-lg"></i><span>Search<br>Blog</span></a>
