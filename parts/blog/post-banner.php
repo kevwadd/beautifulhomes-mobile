c00a10cbbcbab1545d185f641328f4fdeae79aab
Kevin Waddell <kevwaddell@mac.com>
2018-11-16 22:32:50 +0000

    Update post banner to have caption

--- parts/blog/post-banner.php
+++ parts/blog/post-banner.php
@@ -3,10 +3,14 @@
 global $post;
 $feat_img = get_the_post_thumbnail_url($post->ID, 'banner-mb-400-133'); 
 $feat_img_lg = get_the_post_thumbnail_url($post->ID, 'banner-dt-1500-500');
+$caption = get_the_post_thumbnail_caption($post->ID);
 //echo '<pre>';print_r($feat_img_lg);echo '</pre>';
 ?>
 
 <div class="banner-img-slim loading">
 	<div class="bg-img" data-src="<?php echo $feat_img_lg; ?>" style="background-image: url(<?php echo $feat_img; ?>)"></div>
 </div>
+<?php if (!empty($caption)) { ?>
+<div class="banner-caption"><?php echo $caption; ?></div>
+<?php } ?>
 <?php } ?>
