3e6e970e459943f3a8da3685c348f3c90d8ad11d
Kevin Waddell <kevwaddell@mac.com>
2018-12-29 01:22:39 +0000

    Change slim banner

--- parts/blog/post-banner.php
+++ parts/blog/post-banner.php
@@ -1,8 +1,8 @@
 <?php if (has_post_thumbnail()) { ?>
 <?php 
 global $post;
-$feat_img = get_the_post_thumbnail_url($post->ID, 'banner-mb-400-133'); 
-$feat_img_lg = get_the_post_thumbnail_url($post->ID, 'banner-dt-1500-500');
+$feat_img = get_the_post_thumbnail_url($post->ID, 'thumbnail'); 
+$feat_img_lg = get_the_post_thumbnail_url($post->ID, 'medium_large');
 $caption = get_the_post_thumbnail_caption($post->ID);
 //echo '<pre>';print_r($feat_img_lg);echo '</pre>';
 ?>
