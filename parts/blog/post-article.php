a89dd8f0aa4a57a89711151b4a1e5c8d8a1cb45a
Kevin Waddell <kevwaddell@mac.com>
2019-01-11 23:38:49 +0000

    Update category

--- parts/blog/post-article.php
+++ parts/blog/post-article.php
@@ -5,12 +5,14 @@ $feat_img_lg = get_the_post_thumbnail_url($post->ID, 'large');
 $words_by = get_field('words_by');
 $photos_by = get_field('photos_by');
 //$article_imgs = get_field('article_imgs');
+$prim_cat = get_post_meta($post->ID, '_yoast_wpseo_primary_category',true);
+$term = get_category($prim_cat);
 ?>
 <article <?php post_class('single-post'); ?>>
 		<h1 class="headline"><?php the_title(); ?></h1>
 		<div class="post-meta caps">
 			<div><i class="fa fa-calendar"></i>  <?php echo get_the_date( 'F j, Y', $top_post ); ?></div>
-			<div><i class="fa fa-tag"></i>  <?php the_category(','); ?></div>
+			<div><i class="fa fa-tag"></i>  <?php echo $term->name; ?></div>
 			<div><i class="fa fa-pencil"></i> 
 			<?php if (!empty($words_by)) { ?>
 			<?php echo $words_by['display_name']; ?>
